144b4ad653bd7fe939ef685c560d5872bd09a9d6
Hamda Mouhoumed <75206869+xamdoo@users.noreply.github.com>
2022-12-18 15:15:42 +0300

    draft

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,10 +1,15 @@
-function Search() {
+import { useState } from "react";
+
+function Search(props) {
+  const [search, setSearch] = useState('');
+  //handleOnSearch prop passes a function from the parent component (App.js)
+  //whenever search button is clicked the current value of the search input field is called as an argument 
   return (
     <div className="search-section">
       Search for your favorite shows
-      <div classNameName="search-input">
-        <input className="search-box" type="text" placeholder="Search" />
-        <button className="btn-search">Search</button>
+      <div className="search-input">
+        <input className="search-box" type="text" placeholder="Search" onChange={(e)=>setSearch(e.target.value)}/>
+        <button className="btn-search" onClick={()=> props.handleOnSearch(search)}>Search</button>
       </div>
     </div>
   );
